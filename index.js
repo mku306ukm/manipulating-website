63bfb8b3d0fea9ae7daeab20fb15c74a6f74b988
samod singh <samod0098@gmail.com>
2021-05-21 16:48:39 +0530

    manipulating-website

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+let a = window.document;
+
+//  alert("hello umesh")
+
+//  a = prompt("this will distroy your computer.type your name");
+
+//  a = confirm('are you sure you want to delete this page?')
+
+
+// a = window.innerHeight;
+// a = innerWidth;
+// a = scrollX;
+ a = location.toString();
+console.log(a);
